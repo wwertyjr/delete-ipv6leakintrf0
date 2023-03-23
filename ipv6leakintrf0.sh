6d6648ebae22d0bde5b72db75f9b74de9c6ec17c
Werty <66949622+salamiwerty@users.noreply.github.com>
2023-03-23 22:11:48 +0100

    Update, with an a better UI

--- ipv6leakintrf0.sh
+++ ipv6leakintrf0.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-# Definir colores
-COLOR_CYAN='\033[0;36m'
-COLOR_RED='\033[0;31m'
-COLOR_NC='\033[0m'
+clear
 
-# Mostrar mensaje
-echo -e "${COLOR_CYAN}Eliminando ipv6leakintrf0...${COLOR_NC}"
+sleep 1
 
-# Ejecutar comando
-nmcli device delete ipv6leakintrf0
-sudo nmcli connection delete pvpn-ipv6leak-protection
+echo -e "\e[31m
+██ ██████  ██    ██  ██████  ██      ███████  █████  ██   ██ 
+██ ██   ██ ██    ██ ██       ██      ██      ██   ██ ██  ██  
+██ ██████  ██    ██ ███████  ██      █████   ███████ █████   
+██ ██       ██  ██  ██    ██ ██      ██      ██   ██ ██  ██  
+██ ██        ████    ██████  ███████ ███████ ██   ██ ██   ██ 
+\e[33m
+"
 
-# Mostrar mensaje de confirmación
-echo -e "${COLOR_RED}La interfaz ipv6leakintrf0 ha sido eliminada.${COLOR_NC}"
+sleep 1
+echo """By connecting to the internet with PIA VPN, you get IPv6 leak protection. 
+PIA VPN will disable IPv6 traffic to ensure that no IPv6 internet data leaks out from your regular internet connection while connected to the VPN."""
+sleep 1
+echo """This script delete this problem
+
+"""
+echo """[1] Start
+[2] Exit"""
+read num
+
+if [ $num -eq 1 ]
+then
+    clear
+    echo "[!] Starting..."
+    nmcli device delete ipv6leakintrf0
+    sudo nmcli connection delete pvpn-ipv6leak-protection
+    echo -e "\e[32m[!] Done!\e[0m"
+
+
+elif [ $num -eq 2 ]
+then
+    echo "[!] Exiting..."
+    exit
+else
+    echo -e "\e[31m[!] Invalid option!\e[0m"
+fi
